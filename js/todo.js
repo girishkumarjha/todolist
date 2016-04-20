3263f214a86d04df98a9d7c308e951d1162eadfb
Colin Loretz <colin@mechani.sm>
2016-04-20 10:33:28 -0700

    Added javascript todo

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $('#addItem').click(addItem);
+
+  var items = [];
+
+  function addItem(){
+    var listItem = $('#newTodo').val();
+    $('#todos').append('<li><input type = "checkbox" class = "completeItem">' + listItem + '<i class = "glyphicon glyphicon-trash delete"></i></li>');
+    $('#newTodo').val('');
+    $('.delete').click(removeItem);
+    $('.completeItem').change(toggleCompleted);
+  }
+
+  function removeItem(){
+    $(this).parent().remove();
+  }
+
+  function toggleCompleted(){
+    $(this).parent().addClass('completed')
+  }
+
+});
