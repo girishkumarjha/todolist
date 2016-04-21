385b0991c83ff11e9ad42c8f630fe2e33cc3e10c
Colin Loretz <colin@mechani.sm>
2016-04-21 16:09:40 -0700

    Added enter key event

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
   $('#addItem').click(addItem);
   $('#todos').on("change", ".completeItem", toggleCompleted);
   $('#todos').on("click", ".delete", removeItem);
+  $('#newTodo').on("keypress", function (e) {
+    if (e.which == 13) {
+      addItem();
+      e.preventDefault();
+    }
+  });
 
   function addItem(event){
     var listItem = $('#newTodo').val();
